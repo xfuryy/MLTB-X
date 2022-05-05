47c881660a399eafbf7283f161403811bcea5b73
xfuryy <81030085+xfuryy@users.noreply.github.com>
2022-05-05 15:35:49 +0545

    Update aria.sh

--- aria.sh
+++ aria.sh
@@ -7,7 +7,7 @@ aria2c --enable-rpc=true --check-certificate=false --daemon=true \
    --max-connection-per-server=10 --rpc-max-request-size=1024M --quiet=true \
    --bt-stop-timeout=$TORRENT_TIMEOUT --min-split-size=10M --split=10 --allow-overwrite=true \
    --max-overall-download-limit=0 --bt-tracker="[$tracker_list]" --disk-cache=32M \
-   --max-overall-upload-limit=1K --max-concurrent-downloads=15 --summary-interval=0 \
+   --max-overall-upload-limit=1K --max-concurrent-downloads=8 --summary-interval=0 \
    --peer-id-prefix=-qB4400- --user-agent=Wget/1.12 --peer-agent=qBittorrent/4.4.2 \
    --bt-enable-lpd=true --seed-time=0 --max-file-not-found=0 --max-tries=20 \
    --auto-file-renaming=true --reuse-uri=true --http-accept-gzip=true --continue=true \
